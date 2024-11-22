ef27265e080a84f9df71e93a45872ae3c12bee1c
Ryan Vickramasinghe <ryanvickr@gmail.com>
2024-11-21 20:22:40 -0800

    Use loop unrolling when constructing the completed image.

--- src/main.cc
+++ src/main.cc
@@ -60,6 +60,7 @@ int main(int, char**) {
   for (int j = 0; j < image_height; j++) {
     std::clog << "\rScanlines remaining: " << (image_height - j) << ' '
               << std::flush;
+    #pragma unroll
     for (int i = 0; i < image_width; i++) {
       const auto pixel_center =
           pixel00_loc + (unit_vec_u * i) + (unit_vec_v * j);
